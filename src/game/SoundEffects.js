9ef8326d3f849bed0218e7d223f838bad6a838ee
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-05-08 23:44:20 -0400

    fix /assets path with ./assets for proper dist embed for sfx

--- src/game/SoundEffects.js
+++ src/game/SoundEffects.js
@@ -71,37 +71,37 @@ const playWavFile = async (wavDataUri, gainValue = 0.2) => {
 export const playSFX = (type, gainOverride) => {
   switch (type) {
     case "swing":
-      playWavFile("/assets/sfx/sfump.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/sfump.wav", gainOverride ?? 0.2);
       break;
     case "hit":
-      playWavFile("/assets/sfx/hit.wav", gainOverride ?? 0.12);
+      playWavFile("./assets/sfx/hit.wav", gainOverride ?? 0.12);
       break;
     case "jump":
-      playWavFile("/assets/sfx/jump.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/jump.wav", gainOverride ?? 0.2);
       break;
     case "explode":
-      playWavFile("/assets/sfx/explode.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/explode.wav", gainOverride ?? 0.2);
       break;
     case "laser":
-      playWavFile("/assets/sfx/shoot638.wav", gainOverride ?? 0.16);
+      playWavFile("./assets/sfx/shoot638.wav", gainOverride ?? 0.16);
       break;
     case "machine_break":
-      playWavFile("/assets/sfx/machine_break.wav", gainOverride ?? 0.25);
+      playWavFile("./assets/sfx/machine_break.wav", gainOverride ?? 0.25);
       break;
     case "computer_mumble":
-      playWavFile("/assets/sfx/computer_mumble_pickup655.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/computer_mumble_pickup655.wav", gainOverride ?? 0.2);
       break;
     case "hurt":
-      playWavFile("/assets/sfx/hurt_blip668.wav", gainOverride ?? 0.16);
+      playWavFile("./assets/sfx/hurt_blip668.wav", gainOverride ?? 0.16);
       break;
     case "hurtPlayer":
-      playWavFile("/assets/sfx/player_hurt.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/player_hurt.wav", gainOverride ?? 0.2);
       break;
     case "hurtComputer":
-      playWavFile("/assets/sfx/computer_hurt_hit_685.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/computer_hurt_hit_685.wav", gainOverride ?? 0.2);
       break;
     case "death":
-      playWavFile("/assets/sfx/note_music_690.wav", gainOverride ?? 0.2);
+      playWavFile("./assets/sfx/note_music_690.wav", gainOverride ?? 0.2);
       break;
   }
 };
@@ -112,17 +112,17 @@ export const playSFX = (type, gainOverride) => {
  */
 export const preloadSFX = async () => {
   const sounds = [
-    "/assets/sfx/sfump.wav",
-    "/assets/sfx/hit.wav",
-    "/assets/sfx/jump.wav",
-    "/assets/sfx/explosion.wav",
-    "/assets/sfx/shoot638.wav",
-    "/assets/sfx/machine_break.wav",
-    "/assets/sfx/computer_mumble_pickup655.wav",
-    "/assets/sfx/hurt_blip668.wav",
-    "/assets/sfx/player_hurt.wav",
-    "/assets/sfx/computer_hurt_hit_685.wav",
-    "/assets/sfx/note_music_690.wav",
+    "./assets/sfx/sfump.wav",
+    "./assets/sfx/hit.wav",
+    "./assets/sfx/jump.wav",
+    "./assets/sfx/explosion.wav",
+    "./assets/sfx/shoot638.wav",
+    "./assets/sfx/machine_break.wav",
+    "./assets/sfx/computer_mumble_pickup655.wav",
+    "./assets/sfx/hurt_blip668.wav",
+    "./assets/sfx/player_hurt.wav",
+    "./assets/sfx/computer_hurt_hit_685.wav",
+    "./assets/sfx/note_music_690.wav",
   ];
   await Promise.all(sounds.map(getAudioBuffer));
   console.log("All SFX preloaded and cached.");
